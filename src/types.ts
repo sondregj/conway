d8e6144a2e17b9c529aa7ade2e04bc49e7f34c2a
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-11-06 13:32:18 +0100

    :art: Improved types file

--- src/types.ts
+++ src/types.ts
@@ -6,8 +6,6 @@ export interface Board {
     cells: Cell[][]
 }
 
-export type BoardTick = (board: Board, ...args: any[]) => Board
-
+export type BoardTick = (board: Board) => Board
 export type RuleFunction = (board: Board, cell: Cell, x: number, y: number) => boolean
-
 export type NeighborCounter = (board: Board, x: number, y: number) => number
